f728e005d8e92c7ee242a2b767625da853255088
MARUTI PAI <marutipai203@gmail.com>
2025-11-11 00:33:56 +0530

    ci cd integration and ready for training and testing

--- api/main.py
+++ api/main.py
@@ -1,15 +1,14 @@
+# api/main.py
 from fastapi import FastAPI
-import mlflow.sklearn
-import pandas as pd
 from api.models.predict_schema import PredictRequest
 from api.services.predict_services import predict_churn
 
 app = FastAPI(title="Churn Prediction API")
 
-model = mlflow.sklearn.load_model("mlruns/0/<RUN_ID>/artifacts/model")
+@app.get("/health")
+def health():
+    return {"status": "ok"}
 
 @app.post("/predict")
-def predict(data: dict):
-    df = pd.DataFrame([data])
-    predictions = model.predict(df)[0]
-    return {"churn_predictions": bool(predictions)}
+def predict(req: PredictRequest):
+    return predict_churn(req.dict())
